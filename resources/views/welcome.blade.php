2e2d3f0d419daa4c41fa57e13642fc1934136baf
Frankie <frankie@doubleservice.com>
2017-06-25 22:09:58 +0800

    timer ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,79 +1,148 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.4/css/bootstrap2/bootstrap-switch.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-        <title>Timer Orgates </title>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.bootcss.com/vue/2.3.4/vue.min.js"></script>
+    <title>Timer Orgates </title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap.switch/4.0.0-alpha.1/css/bootstrap-switch.min.css">
+    <script src="https://cdn.jsdelivr.net/bootstrap.switch/4.0.0-alpha.1/js/bootstrap-switch.min.js"></script>
+</head>
+<body>
+<div id="main-content" class="container">
+    <h2>
+        <img src="timer.ico" style="max-height: 120px; max-width: 120px">
+        Orgates Timer Service
+    </h2>
+    <p style="color: red">please set the expected time below</p>
 
-            .full-height {
-                height: 100vh;
-            }
+    <div class="make-switch switch-small">
+        Automatic recording time：<input type="checkbox" checked="true" data-checkbox="VALUE1" class="alert-status">
+    </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-    <input type="button" onclick="notifyMe()">
-    </body>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Hour</th>
+            <th>Minute</th>
+            <th>Second</th>
+            <th>
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>
+                <input id="hour" type="text" v-model="hour">
+                <input type="button" value="Return to zero"
+                       class="btn btn-warning" onclick="returnZero('hour')">
+            </td>
+            <td><input id="minute" type="text" v-model="minute">
+                <input type="button" value="Return to zero"
+                       class="btn btn-warning" onclick="returnZero('minute')">
+            </td>
+            <td><input id="second" type="text" v-model="second">
+                <input type="button" value="Return to zero"
+                       class="btn btn-warning" onclick="returnZero('second')">
+            </td>
+            <td>
+                <input type="button" value="setting" class="btn btn-primary"
+                       onclick="settingLoopAlert()">
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
 </html>
 <script>
-    Notification.requestPermission().then(function(result) {
+
+    var name = 'time';
+
+    var vm = new Vue({
+        el: '#main-content',
+        data: {
+            hour: 1,
+            minute: 0,
+            second: 0,
+            state: false
+        }
+    });
+
+    function returnZero(type) {
+        if (type = 'hour') {
+            vm.hour = 0;
+        }
+        if (type = 'minute') {
+            vm.minute = 0;
+        }
+        if (type = 'second') {
+            vm.second = 0;
+        }
+    }
+
+    var notifyEnable = false;
+    var timeOut;
+    function loopAlert() {
+
+        time = convertSecond(vm.hour, vm.minute, vm.second);
+
+        if (time > 1) {
+
+            that = this;
+            if (notifyEnable) {
+                notifyMe();
+            }
+
+            timeOut = setTimeout(function () {
+                that.loopAlert(time)
+            }, time);
+        }
+    }
+
+    function settingLoopAlert() {
+
+        clearTimeout(timeOut);
+
+        notifyEnable = false;
+
+        if (vm.state = true) {
+            storeTime();
+        }
+
+        loopAlert();
+
+        notifyEnable = true;
+    }
+
+
+    Notification.requestPermission().then(function (result) {
         console.log(result);
     });
 
     function notifyMe() {
+
+        var title = "Orgates message";
+
+        var options = {
+            body: "stand up! stand up! stand up!",
+            icon: "timer.ico"
+        }
+
         // Let's check if the browser supports notifications
         if (!("Notification" in window)) {
             alert("This browser does not support system notifications");
@@ -82,7 +151,7 @@
         // Let's check whether notification permissions have already been granted
         else if (Notification.permission === "granted") {
             // If it's okay let's create a notification
-            var notification = new Notification("Hi there!");
+            var notification = new Notification(title, options);
         }
 
         // Otherwise, we need to ask the user for permission
@@ -90,7 +159,7 @@
             Notification.requestPermission(function (permission) {
                 // If the user accepts, let's create a notification
                 if (permission === "granted") {
-                    var notification = new Notification("Hi there!");
+                    var notification = new Notification(title, options);
                 }
             });
         }
@@ -98,5 +167,57 @@
         // Finally, if the user has denied notifications and you
         // want to be respectful there is no need to bother them any more.
     }
+
+    function setCookie(name) {
+        document.cookie = name + "=" +
+                vm.hour + ":" + vm.minute + ":" + vm.second +
+                ";path=/";
+    }
+
+    function getCookie(name) {
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length + 1, c.length);
+            }
+        }
+    }
+
+    function storeTime() {
+
+        setCookie(name);
+        var timeArray = getCookie(name).split(':');
+        putVmTime(timeArray);
+    }
+
+    function putVmTime(timeArray) {
+        vm.hour = timeArray[0];
+        vm.minute = timeArray[1];
+        vm.second = timeArray[2];
+    }
+
+    function convertSecond(hour, minute, second) {
+        return hour * 60 * 60 * 1000 +
+                minute * 60 * 1000 +
+                second * 1000;
+    }
+
+    $(function () {
+        $('.alert-status').bootstrapSwitch('state', true);
+
+        $('.alert-status').on('switchChange.bootstrapSwitch', function (event, state) {
+            vm.state = state;
+        });
+
+        var timeArray = getCookie(name).split(':');
+        putVmTime(timeArray);
+
+    });
 
 </script>
