e7907ed13ca73233df104b0d5cbe1c5d2f381acd
Frankie <frankie@doubleservice.com>
2017-06-26 00:43:33 +0800

    set Facebook share button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,15 @@
     <script src="https://cdn.jsdelivr.net/bootstrap.switch/4.0.0-alpha.1/js/bootstrap-switch.min.js"></script>
 </head>
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="main-content" class="container">
     <h2>
         <img src="timer.ico" style="max-height: 120px; max-width: 120px">
@@ -89,6 +98,13 @@
         </tr>
         </tbody>
     </table>
+    <div class="fb-share-button" data-href="http://standup.orgates.net/"
+         data-layout="button_count" data-size="small"
+         data-mobile-iframe="true">
+        <a class="fb-xfbml-parse-ignore" target="_blank"
+           href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fstandup.orgates.net%2F&amp;src=sdkpreparse">分享</a>
+    </div>
+
 </div>
 </body>
 </html>
