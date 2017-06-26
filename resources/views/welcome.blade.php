d0677af352b398c8e816e1b34b4d929d43de4d0c
Frankie <frankie@doubleservice.com>
2017-06-26 22:41:42 +0800

    修正字

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     {{--facebook start--}}
 
-    <meta name="og:description" content="為了健康設計間單操作方式，及時彈跳視窗，
-    告知辛苦工作的朋友們請起來走走動動"/>
+    <meta name="og:description" content="為了健康設計簡單操作方式，及時彈跳視窗，
+    通知辛苦工作的朋友們請起來走走動動"/>
 
     <meta property="og:title" content="健康stand up 計時器"/>
 
@@ -35,8 +35,8 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="健康計時器"/>
-    <meta name="description" content="為了健康設計間單操作方式，及時彈跳視窗，
-    告知辛苦工作的朋友們請起來走走動動"/>
+    <meta name="description" content="為了健康設計簡單操作方式，及時彈跳視窗，
+    通知辛苦工作的朋友們請起來走走動動"/>
 
 
     <link rel="shortcut icon" href="timer.ico"/>
